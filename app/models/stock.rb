5ba0d838bbb47021c4565a927893d35411ba20d7
PJ Mantoss <joelptoss@gmail>
2019-06-20 19:07:43 +0100

    Add methods to the  stock and user model files and restrictions to the result partial

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
     has_many :user_stocks
     has_many :users, through: :user_stocks
 
+    def self.find_by_ticker(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end
+
     def self.new_from_lookup(ticker_symbol)
         begin
             client=IEX::Api::Client.new(publishable_token:'pk_2c0aeff38fe04a53b5a83568040db1cb')
