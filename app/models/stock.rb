2ec9531689bc0c62524aaaf05e363ac62590f780
PJ Mantoss <joelptoss@gmail>
2019-06-20 18:15:36 +0100

    Add associations to stock and user model files

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
     def self.new_from_lookup(ticker_symbol)
         begin
             client=IEX::Api::Client.new(publishable_token:'pk_2c0aeff38fe04a53b5a83568040db1cb')
